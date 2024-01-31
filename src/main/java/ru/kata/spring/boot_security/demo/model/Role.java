303f7ada0756d8a72510748ce08938f2cf8fdb1f
DYAlex <yvette.demidova@yandex.ru>
2024-01-31 16:20:28 +0300

    Implemented Role and User with interfaces GrantedAuthority and UserDetails respectively

---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,22 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+    @Id
+    private Long id;
+
+    private String name;
+
+    public String getAuthority() {
+        return getName();
+    }
+}
