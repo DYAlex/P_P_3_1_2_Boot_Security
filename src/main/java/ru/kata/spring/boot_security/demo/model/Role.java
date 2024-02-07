75dc60b7252490b09f010ebc28f743e3be805f53
DYAlex <yvette.demidova@yandex.ru>
2024-02-07 17:48:01 +0300

    Changes to Role model

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -1,20 +1,25 @@
 package ru.kata.spring.boot_security.demo.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "role")
 public class Role implements GrantedAuthority {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
 
     public String getAuthority() {
         return getName();
