36dddd2ceffd9070dd12eb55f41ee2724040ef3c
DYAlex <yvette.demidova@yandex.ru>
2024-02-08 15:42:52 +0300

    Changed controllers: split UsersController into RootController, AdminController and UsersController

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RootController.java
@@ -0,0 +1,26 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+public class RootController {
+    private final UserService userService;
+    public RootController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String indexPage(Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("user", userService.loadUserByUsername(principal.getName()));
+        }
+        return "index";
+    }
+
+    // TODO: Add login and logout mappings
+}
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
@@ -2,91 +2,22 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
-import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 public class UsersController {
     private final UserService userService;
-    private final RoleService roleService;
-
-    public UsersController(UserService userService, RoleService roleService) {
+    public UsersController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
     }
 
     @GetMapping()
-    public String indexPage(Model model, Principal principal) {
-        if (principal != null) {
-            model.addAttribute("user", userService.loadUserByUsername(principal.getName()));
-        }
-        return "index";
-    }
-
-    @GetMapping("/user")
     public String userPage(Model model, Principal principal) {
         model.addAttribute("user", userService.loadUserByUsername(principal.getName()));
         return "user";
-    }
-
-    @GetMapping("/admin")
-    public String adminPage(ModelMap model) {
-        List<User> users = userService.findAll();
-        model.addAttribute("users", users);
-        return "admin";
-    }
-
-    @GetMapping("/admin/show")
-    public String show(@RequestParam("id") long id, Model model) {
-        model.addAttribute("user", userService.findById(id));
-        return "/admin/show";
-    }
-
-    @GetMapping("/admin/edit")
-    public String editUserPage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("user", userService.findById(id));
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "/admin/edit";
-    }
-
-    @PutMapping("/admin/edit{id}")
-    public String editUser(@ModelAttribute("user") User user) {
-        userService.updateUser(user.getId(), user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/admin/new")
-    public String createUserForm(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("roles", roleService.getAllRoles());
-//        System.err.println(user.getRoles());
-//        System.err.println(user.isEnabled());
-//        System.err.println(user.isAccountNonLocked());
-//        System.err.println(user.isAccountNonExpired());
-//        System.err.println(user.isCredentialsNonExpired());
-        return "/admin/new";
-    }
-
-    @PostMapping("/admin")
-    public String addUserToDb(@ModelAttribute("user") User user) {
-//        System.err.println(user.getRoles());
-//        System.err.println(user.isEnabled());
-//        System.err.println(user.isAccountNonLocked());
-//        System.err.println(user.isAccountNonExpired());
-//        System.err.println(user.isCredentialsNonExpired());
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping(value = "/admin/delete")
-    public String deleteUser(@RequestParam long id) {
-        userService.deleteById(id);
-        return "redirect:/admin";
     }
 }
