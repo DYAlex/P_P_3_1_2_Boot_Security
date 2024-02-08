6169c9dc74413f90bd9fdbbabc2aaa848482a783
DYAlex <yvette.demidova@yandex.ru>
2024-02-08 10:49:11 +0300

    Changed loadUserByUsername method to return my User

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,16 +1,15 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.kata.spring.boot_security.demo.model.User;
 import java.util.List;
 
 public interface UserService extends UserDetailsService {
     User findById(Long id);
-    User findByUsername(String username);
+//    User findByUsername(String username);
     List<User> findAll();
     void saveUser(User user);
     void updateUser(Long id, User updatedUser);
     void deleteById(Long id);
-    UserDetails loadUserByUsername(String username);
+    User loadUserByUsername(String username);
 }
