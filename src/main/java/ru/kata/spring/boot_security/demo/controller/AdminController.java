c986a3d4ee23d563ed2038db523b085a430a6fa5
DYAlex <yvette.demidova@yandex.ru>
2024-02-10 13:29:46 +0300

    Changed AdminController: removed unneeded mappings

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -32,44 +32,22 @@ public class AdminController {
         return "admin";
     }
 
-    @GetMapping("/show")
-    public String show(@RequestParam("id") long id, Model model) {
-        model.addAttribute("user", userService.findById(id));
-        return "/admin/show";
-    }
-
-    @GetMapping("/edit")
-    public String editUserPage(@RequestParam("id") long id, Model model) {
-        model.addAttribute("user", userService.findById(id));
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "/admin/edit";
-    }
+//    @GetMapping("/show")
+//    public String show(@RequestParam("id") long id, Model model) {
+//        model.addAttribute("user", userService.findById(id));
+//        return "/admin/show";
+//    }
 
     @PutMapping("/edit{id}")
     public String editUser(@ModelAttribute("user") User user) {
+        String pass = user.getPassword();
+        System.err.println("Password is: " + pass + " isEmpty(): " + pass.isEmpty() + " isBlanc(): " + pass.isBlank() + " == null: " + (pass == null));
         userService.updateUser(user.getId(), user);
         return "redirect:/admin";
     }
 
-    @GetMapping("/new")
-    public String createUserForm(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("roles", roleService.getAllRoles());
-//        System.err.println(user.getRoles());
-//        System.err.println(user.isEnabled());
-//        System.err.println(user.isAccountNonLocked());
-//        System.err.println(user.isAccountNonExpired());
-//        System.err.println(user.isCredentialsNonExpired());
-        return "/admin/new";
-    }
-
     @PostMapping()
     public String addUserToDb(@ModelAttribute("user") User user) {
-//        System.err.println(user.getRoles());
-//        System.err.println(user.isEnabled());
-//        System.err.println(user.isAccountNonLocked());
-//        System.err.println(user.isAccountNonExpired());
-//        System.err.println(user.isCredentialsNonExpired());
         userService.saveUser(user);
         return "redirect:/admin";
     }
