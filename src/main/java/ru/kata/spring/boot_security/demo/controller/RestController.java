e9e65f70e3800cbc9bdbf65682cffa0a0dbdad1c
DYAlex <yvette.demidova@yandex.ru>
2024-02-13 23:52:31 +0300

    Added RestController

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RestController.java
@@ -0,0 +1,119 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import ru.kata.spring.boot_security.demo.dto.UserDto;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.util.UserErrorResponse;
+import ru.kata.spring.boot_security.demo.util.UserNotCreatedException;
+import ru.kata.spring.boot_security.demo.util.UserNotFoundException;
+import ru.kata.spring.boot_security.demo.util.UserNotUpdatedException;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api/v1/users")
+public class RestController {
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+    @Autowired
+    public RestController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping()
+    public List<UserDto> getAllUsers() {
+        return userService.findAll().stream().map(this::convertToUserDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUserById(@PathVariable long id) {
+        return convertToUserDto(userService.findById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<HttpStatus> addNewUser(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getFieldErrors().forEach(System.err::println);
+
+            StringBuilder errorMsg = new StringBuilder();
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                errorMsg.append(error.getField())
+                        .append(": ").append(error.getDefaultMessage())
+                        .append(";\n");
+            }
+            throw new UserNotCreatedException(errorMsg.toString());
+        }
+        userService.saveUser(convertToUser(userDto));
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PutMapping("/edit{id}")
+    public ResponseEntity<HttpStatus> editUser(@PathVariable long id, @RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getFieldErrors().forEach(System.err::println);
+
+            StringBuilder errorMsg = new StringBuilder();
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                errorMsg.append(error.getField())
+                        .append(": ").append(error.getDefaultMessage())
+                        .append(";\n");
+            }
+            throw new UserNotUpdatedException(errorMsg.toString());
+        }
+        userService.updateUser(id, convertToUser(userDto));
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUser(@PathVariable long id) {
+        userService.deleteById(id);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<UserErrorResponse> handleException(UserNotFoundException e) {
+        UserErrorResponse userErrorResponse = new UserErrorResponse(
+                "User with this id wasn't found",
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(userErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<UserErrorResponse> handleException(UserNotCreatedException e) {
+        UserErrorResponse userErrorResponse = new UserErrorResponse(
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(userErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<UserErrorResponse> handleException(UserNotUpdatedException e) {
+        UserErrorResponse userErrorResponse = new UserErrorResponse(
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(userErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    private User convertToUser(UserDto userDto) {
+        return modelMapper.map(userDto, User.class);
+    }
+
+    private UserDto convertToUserDto(User user) {
+        return modelMapper.map(user, UserDto.class);
+    }
+}
