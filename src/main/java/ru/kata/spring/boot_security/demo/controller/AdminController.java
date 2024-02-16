fce0b9fefc7b1da41611e97b5ac439a8c1405191
DYAlex <yvette.demidova@yandex.ru>
2024-02-16 14:54:32 +0300

    Changed AdminController: removed unneeded methods and fields

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -17,44 +17,14 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminController {
     private final UserService userService;
-    private final RoleService roleService;
 
-    public AdminController(UserService userService, RoleService roleService) {
+    public AdminController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
     }
 
     @GetMapping()
     public String adminPage(ModelMap model, Principal principal) {
-        List<User> users = userService.findAll();
-        model.addAttribute("users", users);
         model.addAttribute("principal", userService.loadUserByUsername(principal.getName()));
-        model.addAttribute("newUser", new User());
-        model.addAttribute("roles", roleService.getAllRoles());
         return "admin";
-    }
-
-    @PutMapping("/edit{id}")
-    public String editUser(@ModelAttribute("user") User user) {
-        userService.updateUser(user.getId(), user);
-        return "redirect:/admin";
-    }
-
-    @PostMapping()
-    public String addUserToDb(@ModelAttribute("newUser") @Valid User user, BindingResult bindingResult) {
-        System.err.println("Called addUserToDb PostMapping");
-        if (bindingResult.hasErrors()) {
-            System.err.println("User not valid");
-            System.err.println(bindingResult);
-            return "redirect:/admin";
-        }
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping(value = "/delete")
-    public String deleteUser(@RequestParam long id) {
-        userService.deleteById(id);
-        return "redirect:/admin";
     }
 }
